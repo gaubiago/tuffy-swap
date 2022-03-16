11df7b627bd22f83dda33e4577c6cd397180f201
Gaubert <gaubert.vinicius@gmail.com>
2022-03-16 09:10:25 -0700

    Add page with detailed information about an item (#33)
    * Set up the project env. for both client and server (#10)
    
    Install the following modules:
    
    Client side:
    - react (library)
    - axios
    - bootstrap
    
    Server side:
    - express
    
    * Item's own page has been implemented  (#19)
    
    * Build user interface for item's own page
    
    The page covers the following pieces of information:
    
    - item name
    - item photo
    - item price
    - buy button
    - seller avatar/photo
    - seller name
    - item description
    - seller location
    
    The buy button redirects user to checkout page.
    
    * Fix typos in the item description
    
    * Fix typo in the item description

---
+++ client/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import './index.css';
+// import App from './App';
+import Item from './Item';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+import Checkout from "./Checkout";
+
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Item />} />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
+  </Router>,
+  // <React.StrictMode>
+  //   <Item />
+  // </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
