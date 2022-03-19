d30f848713362910b7c3ffb3669af7ea90d20ab5
Aren Lizardo <70218060+delaureon@users.noreply.github.com>
2022-03-19 14:24:00 -0700

    header-buttons
    Home button with log-in and register links

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Button from './Button';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
 
 ReactDOM.render(
+  // <Router>
+  //   <Routes>
+  //     <Route path="/" element={<Button />} />
+  //   </Routes>
+  // </Router>,
   <React.StrictMode>
-    <App />
+    <Button />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
