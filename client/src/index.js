88ca0a570ff63de647709c70c098400c9f9f6ea0
drbloo <jack1234jack5@gmail.com>
2022-03-19 14:02:16 -0700

    Changed index.js to gaubert's index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from './App';
+import Item from "./Item";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Checkout from "./Checkout";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<Item />} />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
+  </Router>,
+  // <React.StrictMode>
+  //   <Item />
+  // </React.StrictMode>,
   document.getElementById("root")
 );
 
