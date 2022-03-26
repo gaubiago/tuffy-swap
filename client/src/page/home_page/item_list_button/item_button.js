36bbfafa4d97ecf8ae001b419835253875293afa
Megan Honda <60487127+mhonda2@users.noreply.github.com>
2022-03-26 10:23:01 -0700

    Feat/item buttom (#21)
    * Set up the project env. for both client and server
    
    Install the following modules:
    
    Client side:
    - react (library)
    - axios
    - bootstrap
    
    Server side:
    - express
    
    * Created a clickable item
    
    * Updated so the formatting looks presentable
    
    * Update to match dev
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Applied file structure
    
    Co-authored-by: Gaubert <gaubert.vinicius@gmail.com>

---
+++ client/src/page/home_page/item_list_button/item_button.js
@@ -0,0 +1,33 @@
+import item_pic from './item_photo/tuffy_2.jpeg';
+import './item_button.css';
+
+function Icon() {
+  return (
+    <div>
+        <a
+          href="https://reactjs.org" // This will need to be modified later
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={item_pic} className="item_pic" alt="pic" />
+
+          <div className="item_info">
+            <p class="small">
+                <div className="text-item">
+                    Tuffy Mascot Stickers <br></br>
+                    $10.00                <br></br>
+                </div>
+            </p>
+            <p class="small">
+                <div className="text-location">
+                    Fullerton, CA
+                </div>
+            </p>
+          </div>
+        </a>
+
+    </div>
+  );
+}
+
+export default Icon;
