16cbdc8ef62bd2ba8b4489a28175dbc6a8f126a1
Gaubert <gaubert.vinicius@gmail.com>
2022-04-30 21:26:27 -0700

    Throw error message if credentials are invalid

---
+++ client/src/pages/reset_password/ResetPassword.js
@@ -0,0 +1,14 @@
+import './ResetPassword.css';
+import Header from '../header/Header';
+
+function ResetPassword() {
+  return (
+    <div>
+      <Header />
+
+      <div className='Wrapper'>Reset Password Page</div>
+    </div>
+  );
+}
+
+export default ResetPassword;
